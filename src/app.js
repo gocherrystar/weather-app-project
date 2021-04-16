f1e9c95230035fad980615a7158ae00217a54af0
gocherrystar <79139792+gocherrystar@users.noreply.github.com>
2021-04-16 11:40:58 +1000

    Added search engine

--- src/app.js
+++ src/app.js
@@ -36,8 +36,20 @@ function displayTemperature(response) {
 
 }
 
-let city = "London";
-let apiKey = "6b1ad2b1489fef19b8767763765b92f8";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
-`;
-axios.get(apiUrl).then(displayTemperature);
+function search(city) {
+    let apiKey = "6b1ad2b1489fef19b8767763765b92f8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric
+    `;
+    axios.get(apiUrl).then(displayTemperature);
+
+}
+
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value);
+}
+
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
