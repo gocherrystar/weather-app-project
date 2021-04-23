d8e7eebcc0ed58a0a5ce3e16e66fd6cf0cf133a9
gocherrystar <79139792+gocherrystar@users.noreply.github.com>
2021-04-23 19:21:58 +1000

    Added Forecast function

--- src/app.js
+++ src/app.js
@@ -13,7 +13,8 @@ function formatDate(timestamp) {
     return `${day} ${hours}:${minutes}`
 }
 
-function displayForecast() {
+function displayForecast(response) {
+    console.log(response.data.daily);
     let forecastElement = document.querySelector("#forecast");
     
     let forecastHTML = `<div class="row">`;
@@ -42,6 +43,14 @@ function displayForecast() {
     forecastElement.innerHTML = forecastHTML;
 }
 
+function getForecast(coordinates) {
+    console.log(coordinates);
+    let apiKey = "6b1ad2b1489fef19b8767763765b92f8";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+    console.log(apiUrl);
+    axios.get(apiUrl).then(displayForecast);
+}
+
 function displayTemperature(response) {
 
     let temperatureElement = document.querySelector("#temperature");
@@ -63,6 +72,7 @@ function displayTemperature(response) {
     iconElement.setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
     iconElement.setAttribute("alt", response.data.weather[0].description);
 
+    getForecast(response.data.coord);
 }
 
 function search(city) {
@@ -108,4 +118,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("London");
-displayForecast();
